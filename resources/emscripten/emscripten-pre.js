b6b98bda4ef78045f43bc2da2b6da181219f91c9
maru <maru@myyahoo.com>
2024-01-06 23:43:11 -0500

    Extra changes to emscripten-pre

--- resources/emscripten/emscripten-pre.js
+++ resources/emscripten/emscripten-pre.js
@@ -1,6 +1,6 @@
 // Note: The `Module` context is already initialized as an
 // empty object by emscripten even before the pre script
-Object.assign(Module, {
+Module = { ...Module,
   preRun: [onPreRun],
   postRun: [],
 
@@ -44,7 +44,7 @@ Object.assign(Module, {
     Module.totalDependencies = Math.max(Module.totalDependencies, left);
     Module.setStatus(left ? `Preparing... (${Module.totalDependencies - left}/${Module.totalDependencies})` : 'Downloading game data...');
   }
-});
+};
 
 var game_pushed = false;
 var lang_pushed = false;
